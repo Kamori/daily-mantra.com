da386a91541242648fa438dd66da55efd6ba93cc
kamorigoat <kamori.goat@gmail.com>
2020-07-25 15:57:00 -0500

    Load mantras properly

--- bin/generate_index.py
+++ bin/generate_index.py
@@ -5,12 +5,13 @@ from random import choice
 
 cur_dir = os.path.dirname(__file__)
 
-MANTRA_LIST = json.load(f"{cur_dir}/../assets/mantras.json")["mantras"]
+with open(f"{cur_dir}/../assets/mantras.json") as mantras:
+    MANTRA_LIST = json.load(mantras)["mantras"]
 
 todays_mantra = choice(MANTRA_LIST)
 
 with open(f"{cur_dir}/../docs/index.html", "w+") as f:
-    f.write(f"Today's Mantra is {todays_mantra}")
+    f.write(f"Today's Mantra is {todays_mantra['quote']}")
 
 
 print("fin")
