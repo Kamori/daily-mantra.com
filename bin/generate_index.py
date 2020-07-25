907eec4f501a1b37b2351a3ee8b0211527206c62
kamorigoat <kamori.goat@gmail.com>
2020-07-25 15:52:37 -0500

    random mantra

--- bin/generate_index.py
+++ bin/generate_index.py
@@ -1,10 +1,16 @@
+import json
 import os
 from datetime import datetime
+from random import choice
 
 cur_dir = os.path.dirname(__file__)
 
+MANTRA_LIST = json.load(f"{cur_dir}/../assets/mantras.json")["mantras"]
+
+todays_mantra = choice(MANTRA_LIST)
+
 with open(f"{cur_dir}/../docs/index.html", "w+") as f:
-    f.write(f"Test {datetime.now()}")
+    f.write(f"Today's Mantra is {todays_mantra}")
 
 
 print("fin")
