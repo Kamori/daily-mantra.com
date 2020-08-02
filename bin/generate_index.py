e8e2a83a748ef5f4edb580fea63f3898449d5b63
kamorigoat <kamori.goat@gmail.com>
2020-08-02 17:24:38 -0500

    Link back to the blog

--- bin/generate_index.py
+++ bin/generate_index.py
@@ -11,7 +11,15 @@ with open(f"{cur_dir}/../assets/mantras.json") as mantras:
 todays_mantra = choice(MANTRA_LIST)
 
 with open(f"{cur_dir}/../docs/index.html", "w+") as f:
-    f.write(f"Today's Mantra is {todays_mantra['quote']}")
+    f.write(f"Today's Mantra is {todays_mantra['quote']}\n")
+    f.write("<br><br>")
+    f.write(
+        "A tool created for <a href='https://scalinghappy.com/'>Scaling Happy</a>"
+    )
+    f.write("<br>")
+    f.write(
+        "Fork me at <a href='https://github.com/Kamori/daily-mantra.com'>GitHub</a>"
+    )
 
 
 print("fin")
