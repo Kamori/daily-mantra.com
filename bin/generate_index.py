ae2cee721592dd536fcb72752114e519f5ad4ce3
kamorigoat <kamori.goat@gmail.com>
2020-08-22 15:56:38 -0500

    Add a simple blue styling, not responsive

--- bin/generate_index.py
+++ bin/generate_index.py
@@ -11,15 +11,44 @@ with open(f"{cur_dir}/../assets/mantras.json") as mantras:
 todays_mantra = choice(MANTRA_LIST)
 
 with open(f"{cur_dir}/../docs/index.html", "w+") as f:
-    f.write(f"Today's Mantra is {todays_mantra['quote']}\n")
-    f.write("<br><br>")
-    f.write(
-        "A tool created for <a href='https://scalinghappy.com/'>Scaling Happy</a>"
-    )
-    f.write("<br>")
-    f.write(
-        "Fork me at <a href='https://github.com/Kamori/daily-mantra.com'>GitHub</a>"
-    )
 
+    f.write(
+        f"""
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Daily Mantra</title>
+    <link rel="stylesheet" href="blue_relax.css" />
+    <link rel="stylesheet" href="foundation-icons/foundation-icons.css" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="//fonts.googleapis.com/css?family=Josefin+Sans"
+    />
+  </head>
+  <body>
+    <div class="circle circle1"></div>
+    <div class="circle circle2"></div>
+    <div class="circle circle3"></div>
+    <div class="circle circle4"></div>
+    <div class="circle circle5"></div>
+    <div class="circle circle6"></div>
+    <div class="circle circle7"></div>
+    <div class="circle circle8"></div>
+    <div class="mantra">
+      {todays_mantra['quote']}
+    </div>
+        <a
+      href="https://github.com/kamori/daily-mantra.com"
+      class="socials"
+      target="_blank"
+      ><i class="fi-social-github"></i> GitHub Project</a
+    >
+  </body>
+</html>
+            """
+    )
 
 print("fin")
